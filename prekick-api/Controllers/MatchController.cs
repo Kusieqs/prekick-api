05dbe71efe05ba681d81ad742986de144a2170f5
kusieqs <Konrad.kus@edu.uekat.pl>
2026-03-18 23:16:20 +0100

    Take the first step to make first endpoint

---
+++ prekick-api/Controllers/MatchController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prekick_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MatchController : ControllerBase
+    {
+        [HttpGet(Name = "GetMatch")]
+        public IEnumerable<Match> GetMatch()
+        {
+            Match m1 = new Match();
+            List<Match> list = new List<Match>();
+            list.Add(m1);
+            return list;
+        }
+    }
+}
---
+++ prekick-api/Match.cs
@@ -0,0 +1,18 @@
+﻿namespace prekick_api
+{
+    public class Match
+    {
+        public DateTime Date { get; set; }
+
+        public Tuple<string, string>? Teams { get; set; }
+
+        public Tuple<int, int>? Scores { get; set; }
+
+        public Match()
+        {
+            Date = DateTime.Now;
+            Teams = Tuple.Create("Test1", "Test2");
+            Scores = Tuple.Create(1,2);
+        }
+    }
+}
